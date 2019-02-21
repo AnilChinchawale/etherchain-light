79f90ced1aef941043947a21d79e757efc3ff0a6
AnilChinchawale <Anil24593@gmail.com>
2019-02-21 14:25:46 +0530

    Config added in Github

---
+++ config.js
@@ -0,0 +1,21 @@
+var web3 = require('web3');
+var net = require('net');
+
+var config = function () {
+  
+  this.logFormat = "combined";
+  // this.ipcPath = process.env["HOME"] + "/.local/share/io.parity.ethereum/jsonrpc.ipc";
+  // this.provider = new web3.providers.IpcProvider(this.ipcPath, net);
+  this.rpcPath = "http://localhost:8545";
+  this.provider = new web3.providers.HttpProvider(this.rpcPath);
+
+  
+  this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
+  
+  this.names = {
+    "0x4f7900282f3d371d585ab1361205b0940ab1789c": "XinFin-MN-001" 
+  }
+  
+}
+
+module.exports = config;
