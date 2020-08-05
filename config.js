2fc0fbb347012937b03d00a7baeae24a36416056
AnilChinchawale <anil24593@gmail.com>
2020-08-05 22:33:13 +0530

    apothem rpc added.

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ var config = function () {
   this.logFormat = "dev";
   // this.ipcPath = process.env["HOME"] + "/.local/share/io.parity.ethereum/jsonrpc.ipc";
   // this.provider = new web3.providers.IpcProvider(this.ipcPath, net);
-  this.rpcPath = "https://testnet.xinfin.network";
+  this.rpcPath = "https://rpc.apothem.network";
   this.provider = new web3.providers.HttpProvider(this.rpcPath);
 
   
   this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
   
   this.names = {
-    "0x4f7900282f3d371d585ab1361205b0940ab1789c": "XinFin-MN-001" 
+    "xdc4f7900282f3d371d585ab1361205b0940ab1789c": "XinFin-MN-001" 
   }
   
 }
